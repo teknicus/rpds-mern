a4996bb70fd89313368b4fd2328ac3801f2c3fa1
“teknicus” <mailme@thomasrob.in>
2020-05-13 16:30:40 +0530

    QR embedded

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+/* import React from "react";
 
 function App() {
-  return <div className="App">He World</div>;
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
+ */
+
+import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import Auth from "./components/layout/Auth";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Auth />
+      </div>
+    );
+  }
 }
 
 export default App;
