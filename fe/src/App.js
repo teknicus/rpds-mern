3979530e7357e526c6088ff51373c7e8af55a93e
“teknicus” <mailme@thomasrob.in>
2020-05-13 09:28:56 +0530

    added bg

--- fe/src/App.js
+++ fe/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import Test from "./qr";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Helo World</h1>
-      <Test />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
