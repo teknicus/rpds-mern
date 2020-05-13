e6a9f41eeb12a1b371e7b12df0f1234a4f6968b2
“teknicus” <mailme@thomasrob.in>
2020-05-13 09:10:12 +0530

    QR done

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Test from "./qr";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Helo World</h1>
+      <Test />
     </div>
   );
 }
